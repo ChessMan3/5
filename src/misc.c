90a8e53ffae97c6e8b6eafb84a9e42f8a0ff4f13
ChessMan3 <amult@ukr.net>
2017-03-09 17:15:01 +0200

    CChess_B3

--- src/misc.c
+++ src/misc.c
@@ -108,7 +108,7 @@ void print_engine_info(int to_uci)
 {
   char my_date[64];
 
-  printf("Cfish %s", Version);
+  printf("CChess_B3 %s", Version);
 
   if (strlen(Version) == 0) {
     int day, month, year;
